cfd9de2d436ce6e465376e4d74c7863bd10671cc
Matheus Tanaka <matheustanakanog@gmail.com>
2023-11-08 21:52:51 +0000

    add CRUD user

--- flask-demo/main.py
+++ flask-demo/main.py
@@ -2,28 +2,50 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Array para armazenar as palavras
-palavras = []
+# Array para armazenar os usuários. OBS: Isso não é um banco de dados de verdade.
+banco_de_dados = []
 
-@app.route('/adicionar_palavra', methods=['POST'])
-def adicionar_palavra():
+@app.route('/adicionar_usuario', methods=['POST'])
+def adicionar_usuario():
     data = request.get_json()
-    palavra = data.get('palavra', '').strip()
+    usuario = data.get('usuario', '')
 
-    # Validando a entrada
-    if not palavra:
-        return jsonify({"erro": "Por favor, insira uma palavra válida!"}), 400
+    # Validando a entrada que será enviada pelo Postman
+    if not usuario:
+        return jsonify({"erro": "Por favor, insira um usuário válido!"}), 400
+    # Adicionando o usuário no banco de dados (array)
+    banco_de_dados.append(usuario)
+    
+    return jsonify({"mensagem": "Usuário adicionado com sucesso!"}), 200
 
-    palavras.append(palavra)
 
-    return jsonify({"mensagem": "Palavra adicionada com sucesso!"}), 200
+@app.route('/listar_usuarios', methods=['GET'])
+def listar_usuarios():
+    # Loop através do array para criar uma lista de usuários
+    lista = [usuario for usuario in banco_de_dados]
 
-@app.route('/listar_palavras', methods=['GET'])
-def listar_palavras():
-    # Loop através do array para criar uma lista de palavras
-    lista = [palavra for palavra in palavras]
+    return jsonify({"usuario": lista}), 200
 
-    return jsonify({"palavras": lista}), 200
+
+@app.route('/buscar_usuario/<usuario>', methods=['GET'])
+def buscar_usuario(usuario):
+
+    if usuario in banco_de_dados:
+        return jsonify({"usuario": usuario}), 200
+    else:
+        return jsonify({"erro": "Usuário não encontrado!"}), 404
+
+
+@app.route('/deletar_usuario/<usuario>', methods=['DELETE'])
+def deletar_usuario(usuario):
+    if usuario in banco_de_dados:
+        # Removendo o usuário do banco de dados
+        banco_de_dados.remove(usuario)
+
+        return jsonify({"mensagem": "Usuário deletado com sucesso!"}), 200
+
+    else:
+        return jsonify({"erro": "Usuário não encontrado!"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
