09f0778a16488df52d8c51a0b4dc5720b2ef86b2
Matheus Tanaka <matheustanakanog@gmail.com>
2023-11-29 23:34:53 +0000

    refactor

--- chatbot/backend/main.py
+++ chatbot/backend/main.py
@@ -5,7 +5,8 @@ from werkzeug.utils import secure_filename
 # Langchain imports
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
-from langchain.document_loaders import UnstructuredExcelLoader
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain.agents.agent_types import AgentType
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
@@ -25,7 +26,7 @@ os.makedirs(os.path.dirname(UPLOAD_FOLDER), exist_ok=True)
 app = Flask(__name__)
 cors = CORS(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx', 'xlsx' }
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx', 'csv' }
 
 file_uploaded = None
 
@@ -33,7 +34,7 @@ def allowed_file(filename):
     return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/uploader", methods = ['POST'])
+@app.route('/uploader', methods = ['POST'])
 @cross_origin()
 def upload_file():
     global file_uploaded
@@ -41,74 +42,108 @@ def upload_file():
     file = request.files['file']
 
     if file and allowed_file(file.filename):
-        print("file request", file)
+        print('file request', file)
         filename = secure_filename(file.filename)
-        print("nome do arquivo", filename)
+        print('nome do arquivo', filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # Juntando o caminho do diretório com o nome do arquivo
         file_save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file_uploaded = file_save_path
-        print("Vendo o tipo do arquivo", file.filename)
 
         if os.path.exists(file_uploaded):
-            print("pasta do arquivo", app.config['UPLOAD_FOLDER'])
-            return jsonify({ "message": "File uploaded successfully", "filename": filename}), 200
+            print('pasta do arquivo: ', app.config['UPLOAD_FOLDER'])
+            return jsonify({ 'message': 'File uploaded successfully', 'filename': filename}), 200
         else: 
-            return jsonify({ "error": "File not found"}), 404
+            return jsonify({ 'error': 'File not found'}), 404
     else:
-        return jsonify({ "error": "File not allowed"}), 400
+        return jsonify({ 'error': 'File not allowed'}), 400
 
 
-def load_document(filename):
-    extension = filename.rsplit('.', 1)[1].lower()
-
-    match extension:
-        case 'docx':
-            loader = Docx2txtLoader(filename)
-            print("Carregando documento docx...")
-        case 'pdf':
-            loader = PyPDFLoader(filename, extract_images=True)
-            print("Carregando documento PDF...")
-        case _:
-            raise ValueError(f"Tipo de arquivo não suportado: {extension}")
-
-    return loader.load()
-
-
-@app.route("/chatbot", methods = ['POST'])
-@cross_origin()
-def send_message():
-    data_question = request.data # Ignora o tipo do conteudo
-    ask = data_question.decode('utf-8')
-
-    print("Carregando o documento")
-    document_data = load_document(file_uploaded)
-
-    api_key = os.getenv('OPENAI_API_KEY')
-
-    print("Separando o documento em pedaços")
+def process_document(data, ask, api_key):
+    print('Separando o documento em pedaços...')
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
 
-    all_splits = text_splitter.split_documents(document_data)
-    print("Documentos quebrados", all_splits)
+    all_splits = text_splitter.split_documents(data)
+    print('Documentos separados: \n', all_splits)
 
-    print("Salvando os documentos no vetor")
+    print('Salvando os documentos no vetor...')
     vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
 
-    print("Configurando o chatbot")
-    llm = ChatOpenAI(openai_api_key=api_key, model_name='gpt-4', temperature=0.7)
-    memory = ConversationSummaryMemory(llm=llm, memory_key="chat_history", return_messages=True)
+    print('Configurando o chatbot...')
+    llm = ChatOpenAI(openai_api_key=api_key, model_name='gpt-3.5-turbo-0613', temperature=0.7)
+    memory = ConversationSummaryMemory(llm=llm, memory_key='chat_history', return_messages=True)
     retriever = vectorstore.as_retriever()
 
-    print("Realizando a pergunta")
+    print('[PERGUNTA]: ', ask)
     qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
     response = qa(ask)
 
     answer = response.get('answer', 'No answer found')
 
-    print("Resposta do chatgpt:", answer)
+    print('[RESPOSTA]: \n', answer)
 
-    return jsonify({ "answer": answer }), 200 
+    response_json = jsonify({ 'answer': answer }), 200 
+ 
+    return response_json 
 
-if __name__ == "__main__":
+
+def process_csv(filename, ask, api_key): 
+
+    print('Carregando documento csv...')
+
+    agent = create_csv_agent(
+        ChatOpenAI(temperature=0, model='gpt-3.5-turbo-0613', openai_api_key=api_key),
+        filename,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+    )
+
+    print('[PERGUNTA]: ', ask)
+    response = agent.run(ask)
+    answer = response
+    print('[RESPOSTA]: \n', answer)
+
+    response_json = jsonify({ 'answer': answer }), 200 
+ 
+    return response_json 
+
+
+def check_file_extension(filename, ask):
+    api_key = os.getenv('OPENAI_API_KEY')
+    extension = filename.rsplit('.', 1)[1].lower()
+
+    match extension:
+        case 'csv':
+            print('Carregando documento CSV...')
+
+            return process_csv(filename, ask, api_key)
+        case 'docx' | 'pdf':
+            if extension == 'docx':
+                print('Carregando documento DOCX...')
+
+                loader = Docx2txtLoader(filename)
+                data = loader.load()
+
+            else:
+                print('Carregando documento PDF...')
+
+                loader = PyPDFLoader(filename, extract_images=False)
+                data = loader.load()
+
+            return process_document(data, ask, api_key) 
+
+        case _:
+            raise ValueError(f'Tipo de arquivo não suportado: {extension}')
+
+
+
+@app.route('/chatbot', methods = ['POST'])
+@cross_origin()
+def send_message():
+    data_question = request.data
+    ask = data_question.decode('utf-8')
+
+    return check_file_extension(filename=file_uploaded, ask=ask)    
+
+if __name__ == '__main__':
     app.run(debug=True)
